b5454f1356223e19c3f6ee8ed9f530a57035a963
Nguyen Xuan Anh <SimonNgj@users.noreply.github.com>
2018-05-22 14:46:55 +1000

    Update Write.py

--- Write.py
+++ Write.py
@@ -1,8 +1,7 @@
 import numpy as np
 import cv2
-import argparse
+#import argparse
 from collections import deque
-
 
 cap=cv2.VideoCapture(0)
 
@@ -10,21 +9,22 @@ pts = deque(maxlen=64)
 
 Lower_green = np.array([110,50,50])
 Upper_green = np.array([130,255,255])
+
 while True:
-	ret, img=cap.read()
-	hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-	kernel=np.ones((5,5),np.uint8)
-	mask=cv2.inRange(hsv,Lower_green,Upper_green)
+	ret, img = cap.read()
+	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	kernel = np.ones((5,5),np.uint8)
+	mask = cv2.inRange(hsv, Lower_green, Upper_green)
 	mask = cv2.erode(mask, kernel, iterations=2)
-	mask=cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernel)
-	#mask=cv2.morphologyEx(mask,cv2.MORPH_CLOSE,kernel)
+	mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN,kernel)
+	mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE,kernel)
 	mask = cv2.dilate(mask, kernel, iterations=1)
-	res=cv2.bitwise_and(img,img,mask=mask)
-	cnts,heir=cv2.findContours(mask.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2:]
+	res = cv2.bitwise_and(img, img, mask=mask)
+	cnts, heir = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2:]
 	center = None
  
 	if len(cnts) > 0:
-		c = max(cnts, key=cv2.contourArea)
+		c = max(cnts, key = cv2.contourArea)
 		((x, y), radius) = cv2.minEnclosingCircle(c)
 		M = cv2.moments(c)
 		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
@@ -42,13 +42,11 @@ while True:
 		
 	
 	cv2.imshow("Frame", img)
-	cv2.imshow("mask",mask)
+	#cv2.imshow("mask",mask)
 	cv2.imshow("res",res)
 	
-	
-	k=cv2.waitKey(30) & 0xFF
-	if k==32:
-		break
+	if cv2.waitKey(1) & 0xFF == ord('q'): break
+    
 # cleanup the camera and close any open windows
 cap.release()
 cv2.destroyAllWindows()
